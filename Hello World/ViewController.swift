192ef94bcf2adeb536efffd0f57b87e205ef6e18
Jeffrey D Huskey <jdhuskey@mac.com>
2016-10-03 16:13:38 -0400

    Initial commit

--- Hello World/ViewController.swift
+++ Hello World/ViewController.swift
@@ -10,14 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var background: UIImageView!
+    @IBOutlet var titleImage: UIImageView!
+    @IBOutlet var welcomeButton: UIButton!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func welcomeButton(_ sender: AnyObject) {
+        
+        titleImage.isHidden = false
+        background.isHidden = false
+        welcomeButton.isHidden = true
+        
     }
 
 
